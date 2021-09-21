4e7334a858008d75122c7d2d3877693ed7069a2f
celikcanss <90218477+celikcanss@users.noreply.github.com>
2021-09-21 19:17:54 +0300

    week2homework

--- src/week1homework/SalaryCalculator.java
+++ src/week1homework/SalaryCalculator.java
@@ -17,17 +17,28 @@ public class SalaryCalculator {
         scanner.close();
 
         double unitSalary = baseSalary / 40;
+        double totalSalary ;
 
-        if (baseSalary<1000)
+        if (baseSalary<1000) {
+            totalSalary = 0;
             System.out.println("The base salary is invalid, please enter a value at least 1000. ");
-        else if (weekdayHours>=40 && weekendHours>10)
-            System.out.println("Weekly salary is:  " + ((40*unitSalary+((weekdayHours-40)*1.5*unitSalary)+weekendHours*2*unitSalary)+500));
-        else if (weekdayHours>=40)
-            System.out.println("Weekly salary is:  " + (40*unitSalary+((weekdayHours-40)*1.5*unitSalary)+weekendHours*2*unitSalary));
-        else if (weekdayHours + weekendHours > 40)
-            System.out.println("Weekly salary is:  " + (weekdayHours*unitSalary + (40-weekdayHours)*unitSalary +(weekdayHours+weekendHours-40)*unitSalary*2));
-        else
-        System.out.println("Weekly salary is:  " + (weekdayHours + weekendHours)*unitSalary);
+        }
+        else if (weekdayHours>=40 && weekendHours>10){
+           totalSalary= ((40*unitSalary+((weekdayHours-40)*1.5*unitSalary)+weekendHours*2*unitSalary)+500);
+        }
+        else if (weekdayHours>=40){
+            totalSalary= (40*unitSalary+((weekdayHours-40)*1.5*unitSalary)+weekendHours*2*unitSalary);
+        }
+        else if (weekdayHours + weekendHours > 40){
+          totalSalary= (weekdayHours*unitSalary + (40-weekdayHours)*unitSalary +(weekdayHours+weekendHours-40)*unitSalary*2);
+        }
+        else{
+        totalSalary=(weekdayHours + weekendHours)*unitSalary;
+        }
+
+        if (totalSalary>0){
+            System.out.println("Total salary is: " + totalSalary);
+   }
 
 
 
@@ -40,12 +51,7 @@ public class SalaryCalculator {
 
 
 
+   }
 
 
-
-
-
-    }
-
-
-}
+   }
