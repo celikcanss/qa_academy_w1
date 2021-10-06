89f996ae8d677f33e245a2f196c6a6a7dfbac9a9
celikcanss <90218477+celikcanss@users.noreply.github.com>
2021-10-06 23:38:43 +0300

    week2homework

--- src/week2homework/SalaryRaise.java
+++ src/week2homework/SalaryRaise.java
@@ -1,31 +1,91 @@
 package week2homework;
 
-public class SalaryRaise {
+import java.util.Scanner;
 
-    public static double salaryRaise(double salary, int year) {
-        double raisedSalary;
-        if (year<1){
-            raisedSalary=salary;
+public class SalaryRaise {
+    public static double salaryCalculator (){
+
+        System.out.println("Please enter base salary.");
+        Scanner scanner = new Scanner(System.in);
+        int baseSalary = scanner.nextInt();
+        System.out.println("Please enter weekday working hours.");
+        Scanner scanner2 = new Scanner(System.in);
+        int weekdayHours = scanner.nextInt();
+        System.out.println("Please enter weekend working hours.");
+        Scanner scanner3 = new Scanner(System.in);
+        int weekendHours = scanner.nextInt();
+
+        scanner.close();
+
+        double unitSalary = baseSalary / 40;
+        double totalSalary ;
+
+        if (baseSalary<1000) {
+            totalSalary = 0;
+            System.out.println("The base salary is invalid, please enter a value at least 1000. ");
         }
-        else if (year==1 ){
-            raisedSalary=salary*1.05;
+        else if (weekdayHours>=40 && weekendHours>10){
+            totalSalary= ((40*unitSalary+((weekdayHours-40)*1.5*unitSalary)+weekendHours*2*unitSalary)+500);
+        }
+        else if (weekdayHours>=40){
+            totalSalary= (40*unitSalary+((weekdayHours-40)*1.5*unitSalary)+weekendHours*2*unitSalary);
+        }
+        else if (weekdayHours + weekendHours > 40){
+            totalSalary= (weekdayHours*unitSalary + (40-weekdayHours)*unitSalary +(weekdayHours+weekendHours-40)*unitSalary*2);
+        }
+        else{
+            totalSalary=(weekdayHours + weekendHours)*unitSalary;
+        }
+
+        if (totalSalary>0){
+            System.out.println("Total salary is: " + totalSalary);
+        }
+
+
+        return totalSalary;
+    }
+
+
+
+
+    public static void raiseSalary(){
+        System.out.println("Please enter year.");
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        double raisedSalary = 0;
+        double totalSalary=salaryCalculator();
+        if(year==0){
+            raisedSalary=totalSalary;
+        }
+        else if (year==1){
+            raisedSalary=totalSalary*1.05;
         }
         else if (year==2){
-            raisedSalary=salary*1.1;
+            raisedSalary=totalSalary*1.1;
         }
-        else if (year==3) {
-            raisedSalary = salary * 1.15;
+        else if (year==3){
+            raisedSalary=totalSalary*1.15;
         }
-        else if(year==4){
-            raisedSalary=salary*1.2;
+        else if (year==4){
+            raisedSalary=totalSalary*1.20;
         }
-        else {
-            raisedSalary = salary * 1.25;
+        else if (year>=5){
+            raisedSalary=totalSalary*1.25;
         }
-        return raisedSalary;
+
+        System.out.println("Final salary is: " + raisedSalary);
+    }
+
+
+
+
+
+    public static void main(String[]args){
+        raiseSalary();
 
     }
-    public static void main(String[] args){
-        System.out.println("Raised salary is: " + salaryRaise(1000, 5));
-    }
+
+
+
+
 }
